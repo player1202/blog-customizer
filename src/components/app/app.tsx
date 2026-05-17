0d2f72d67270ae49015f641212e53460429c5275
player1202 <gec_igor@mail.ru>
2026-05-17 16:56:24 +0500

    4

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,17 +4,11 @@ import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
 import { defaultArticleState, ArticleStateType } from '../../constants/articleProps';
 import styles from './app.module.scss';
-
 export const App = () => {
-  const [appliedSettings, setAppliedSettings] = useState<ArticleStateType>(defaultArticleState);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [settings, setSettings] = useState<ArticleStateType>(defaultArticleState);
 
-  const handleApplySettings = (settings: ArticleStateType) => {
-    setAppliedSettings(settings);
-  };
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+  const handleSettingsChange = (newSettings: ArticleStateType) => {
+    setSettings(newSettings);
   };
 
   return (
@@ -22,20 +16,16 @@ export const App = () => {
       className={clsx(styles.main)}
       style={
         {
-          '--font-family': appliedSettings.fontFamilyOption.value,
-          '--font-size': appliedSettings.fontSizeOption.value,
-          '--font-color': appliedSettings.fontColor.value,
-          '--container-width': appliedSettings.contentWidth.value,
-          '--bg-color': appliedSettings.backgroundColor.value,
+          '--font-family': settings.fontFamilyOption.value,
+          '--font-size': settings.fontSizeOption.value,
+          '--font-color': settings.fontColor.value,
+          '--container-width': settings.contentWidth.value,
+          '--bg-color': settings.backgroundColor.value,
         } as CSSProperties
       }
     >
       <Article />
-      <ArticleParamsForm
-        isOpen={isSidebarOpen}
-        onToggle={toggleSidebar}
-        onApply={handleApplySettings}
-      />
+      <ArticleParamsForm onSettingsChange={handleSettingsChange} />
     </main>
   );
 };
